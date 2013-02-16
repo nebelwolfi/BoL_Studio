34f9df290932aff03f51b2baa20df477b45400e5
SurfaceS <surfaces@none>
2013-02-16 20:41:37 +0100

    remove shroom if exploded, added wards

--- Scripts/hiddenObjects.lua
+++ Scripts/hiddenObjects.lua
@@ -12,60 +12,71 @@
 	v0.1c				change spells names for teemo
 	v0.1d				fix the perma show
 	v0.2				BoL Studio Version
+	v0.3				Reworked
+	-- todo : maybe add zira ?
 	
 	USAGE :
 	Hold shift key to see the hidden object's range.
 ]]
 
 do
-	local hiddenObjects = {
 		--[[      CONFIG      ]]
-		showOnMiniMap = true,			-- show objects on minimap
-		useSprites = true,				-- show sprite on minimap
+		showOnMiniMap = true			-- show objects on minimap
+		useSprites = false				-- show sprite on minimap
 		--[[      GLOBAL      ]]
 		objectsToAdd = {
 			{ name = "VisionWard", objectType = "wards", spellName = "VisionWard", charName = "VisionWard", color = 0x00FF00FF, range = 1450, duration = 180000, sprite = "yellowPoint"},
 			{ name = "SightWard", objectType = "wards", spellName = "SightWard", charName = "SightWard", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
+			{ name = "ItemGhostWard", objectType = "wards", spellName = "VisionWard", charName = "SightWard", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
+			{ name = "ItemMiniWard", objectType = "wards", spellName = "VisionWard", charName = "SightWard", color = 0x00FF00FF, range = 1450, duration = 60000, sprite = "greenPoint"},
 			{ name = "WriggleLantern", objectType = "wards", spellName = "WriggleLantern", charName = "WriggleLantern", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
 			{ name = "Jack In The Box", objectType = "boxes", spellName = "JackInTheBox", charName = "ShacoBox", color = 0x00FF0000, range = 300, duration = 60000, sprite = "redPoint"},
 			{ name = "Cupcake Trap", objectType = "traps", spellName = "CaitlynYordleTrap", charName = "CaitlynTrap", color = 0x00FF0000, range = 300, duration = 240000, sprite = "cyanPoint"},
 			{ name = "Noxious Trap", objectType = "traps", spellName = "Bushwhack", charName = "Nidalee_Spear", color = 0x00FF0000, range = 300, duration = 240000, sprite = "cyanPoint"},
 			{ name = "Noxious Trap", objectType = "traps", spellName = "BantamTrap", charName = "TeemoMushroom", color = 0x00FF0000, range = 300, duration = 600000, sprite = "cyanPoint"},
+			{ name = "Seed", objectType = "traps", spellName = "ZyraSeed", charName = "ZyraSeed", color = 0x00FF0000, range = 300, duration = 30000, sprite = "cyanPoint"},
 			-- to confirm spell
 			{ name = "DoABarrelRoll", objectType = "boxes", spellName = "MaokaiSapling2", charName = "MaokaiSproutling", color = 0x00FF0000, range = 300, duration = 35000, sprite = "redPoint"},
-		},
-		tmpObjects = {},
+		}
+		tmpObjects = {}
 		sprites = {
 			cyanPoint = { spriteFile = "PingMarkerCyan_8", }, 
 			redPoint = { spriteFile = "PingMarkerRed_8", }, 
 			greenPoint = { spriteFile = "PingMarkerGreen_8", }, 
 			yellowPoint = { spriteFile = "PingMarkerYellow_8", },
 			greyPoint = { spriteFile = "PingMarkerGrey_8", },
-		},
-		objects = {},
-	}
+		}
+		objects = {}
 
 	--[[      CODE      ]]
-	function hiddenObjects.objectExist(spellName, pos, tick)
-		for i,obj in pairs(hiddenObjects.objects) do
+	function objectExist(spellName, pos, tick)
+		for i,obj in pairs(objects) do
 			if obj.object == nil and obj.spellName == spellName and GetDistance(obj.pos, pos) < 200 and tick < obj.seenTick then
 				return i
 			end
 		end	
 		return nil
 	end
-
-	function hiddenObjects.addObject(objectToAdd, pos, fromSpell, object)
+	
+	function removeObjectCasted(object, name)
+		for i,obj in pairs(objects) do
+			if obj.charName == name and GetDistance(obj.pos, object) < 200 then
+				objects[i] = nil
+			end
+		end	
+	end
+	
+	function addObject(objectToAdd, pos, fromSpell, object)
 		-- add the object
 		local tick = GetTickCount()
 		local objId = objectToAdd.spellName..(math.floor(pos.x) + math.floor(pos.z))
 		--check if exist
-		local objectExist = hiddenObjects.objectExist(objectToAdd.spellName, {x = pos.x, z = pos.z,}, tick - 2000)
+		local objectExist = objectExist(objectToAdd.spellName, {x = pos.x, z = pos.z,}, tick - 2000)
 		if objectExist ~= nil then
 			objId = objectExist
 		end
-		if hiddenObjects.objects[objId] == nil then
-			hiddenObjects.objects[objId] = {
+		if objects[objId] == nil then
+			objects[objId] = {
 				object = object,
 				color = objectToAdd.color,
 				range = objectToAdd.range,
@@ -77,31 +88,43 @@ do
 				visible = (object == nil),
 				display = { visible = false, text = ""},
 			}
-		elseif hiddenObjects.objects[objId].object == nil and object ~= nil and object.valid then
-			hiddenObjects.objects[objId].object = object
-			hiddenObjects.objects[objId].fromSpell = false
+			if (object ~= nil and object.mana ~= 0) then
+				objects[objId].endTick = tick + object.mana
+			end
+		elseif objects[objId].object == nil and object ~= nil and object.valid then
+			objects[objId].object = object
+			objects[objId].fromSpell = false
 		end
-		hiddenObjects.objects[objId].pos = {x = pos.x, y = pos.y, z = pos.z, }
-		if hiddenObjects.showOnMiniMap == true then hiddenObjects.objects[objId].minimap = GetMinimap(pos) end
+		objects[objId].pos = {x = pos.x, y = pos.y, z = pos.z, }
+		if showOnMiniMap == true then objects[objId].minimap = GetMinimap(pos) end
 	end
 
 	function OnCreateObj(object)
-		if object ~= nil and object.valid and object.type == "obj_AI_Minion" then
-			for i,objectToAdd in pairs(hiddenObjects.objectsToAdd) do
-				if object.name == objectToAdd.name then
-					local tick = GetTickCount()
-					table.insert(hiddenObjects.tmpObjects, {tick = tick, object = object})
+		if object ~= nil and object.valid then
+			if object.type == "obj_AI_Minion" then
+				for i,objectToAdd in pairs(objectsToAdd) do
+					if object.name == objectToAdd.name and object.charName == objectToAdd.charName then
+						local tick = GetTickCount()
+						table.insert(tmpObjects, {tick = tick, object = object})
+					end
 				end
+			elseif object.name == "ShroomMine.troy" then
+				removeObjectCasted(object, "TeemoMushroom")
+				--removeObjectCasted(object, "MaokaiSproutling")
+				--removeObjectCasted(object, "Nidalee_Spear")
+				--removeObjectCasted(object, "CaitlynTrap")
+				--removeObjectCasted(object, "ShacoBox")
+				--removeObjectCasted(object, "ZyraSeed")
 			end
 		end
 	end
 
 	function OnProcessSpell(object,spell)
 		if object ~= nil and object.valid and object.team == TEAM_ENEMY then
-			for i,objectToAdd in pairs(hiddenObjects.objectsToAdd) do
+			for i,objectToAdd in pairs(objectsToAdd) do
 				if spell.name == objectToAdd.spellName then
 					ticked = GetTickCount()
-					hiddenObjects.addObject(objectToAdd, spell.endPos, true)
+					addObject(objectToAdd, spell.endPos, true)
 				end
 			end
 		end
@@ -109,12 +132,12 @@ do
 
 	function OnDeleteObj(object)
 		if object ~= nil and object.valid and object.name ~= nil and object.type == "obj_AI_Minion" then
-			for i,objectToAdd in pairs(hiddenObjects.objectsToAdd) do
+			for i,objectToAdd in pairs(objectsToAdd) do
 				if object.charName == objectToAdd.charName then
 					-- remove the object
-					for j,obj in pairs(hiddenObjects.objects) do
+					for j,obj in pairs(objects) do
 						if obj.object ~= nil and obj.object.valid and obj.object.networkID ~= nil and obj.object.networkID == object.networkID then
-							hiddenObjects.objects[j] = nil
+							objects[j] = nil
 							return
 						end
 					end
@@ -126,14 +149,14 @@ do
 	function OnDraw()
 		if gameState:gameIsOver() then return end
 		local shiftKeyPressed = IsKeyDown(16)
-		for i,obj in pairs(hiddenObjects.objects) do
+		for i,obj in pairs(objects) do
 			if obj.visible == true then
 				DrawCircle(obj.pos.x, obj.pos.y, obj.pos.z, 100, obj.color)
 				DrawCircle(obj.pos.x, obj.pos.y, obj.pos.z, (shiftKeyPressed and obj.range or 200), obj.color)
 				--minimap
-				if hiddenObjects.showOnMiniMap == true then
-					if hiddenObjects.useSprites then
-						hiddenObjects.sprites[obj.sprite].sprite:Draw(obj.minimap.x, obj.minimap.y, 0xFF)
+				if showOnMiniMap == true then
+					if useSprites then
+						sprites[obj.sprite].sprite:Draw(obj.minimap.x, obj.minimap.y, 0xFF)
 					else
 						DrawText("o",31,obj.minimap.x-7,obj.minimap.y-13,obj.color)
 					end
@@ -148,22 +171,22 @@ do
 	function OnTick()
 		if gameState:gameIsOver() then return end
 		local tick = GetTickCount()
-		for i,obj in pairs(hiddenObjects.tmpObjects) do
-			if tick > obj.tick + 1000 or obj.object == nil or obj.object.team == player.team then
-				hiddenObjects.tmpObjects[i] = nil
+		for i,obj in pairs(tmpObjects) do
+			if tick > obj.tick + 1000 or obj.object == nil or not obj.object.valid or obj.object.team == player.team then
+				tmpObjects[i] = nil
 			else
-				for j,objectToAdd in pairs(hiddenObjects.objectsToAdd) do
+				for j,objectToAdd in pairs(objectsToAdd) do
 					if obj.object ~= nil and obj.object.valid and obj.object.charName == objectToAdd.charName and obj.object.team == TEAM_ENEMY then
-						hiddenObjects.addObject(objectToAdd, obj.object, false, obj.object)
-						hiddenObjects.tmpObjects[i] = nil
+						addObject(objectToAdd, obj.object, false, obj.object)
+						tmpObjects[i] = nil
 						break
 					end
 				end
 			end
 		end
-		for i,obj in pairs(hiddenObjects.objects) do
-			if tick > obj.endTick or (obj.object ~= nil) and false then
-				hiddenObjects.objects[i] = nil
+		for i,obj in pairs(objects) do
+			if tick > obj.endTick or (obj.object ~= nil and obj.object.valid and obj.object.team == player.team) then
+				objects[i] = nil
 			else
 				if obj.object == nil or obj.object.valid == false or (obj.object ~= nil and obj.object.valid and obj.object.dead ~= nil and obj.object.dead == false) then
 				--if obj.object == nil
@@ -188,8 +211,8 @@ do
 
 	function OnLoad()
 		gameState = GameState()
-		if hiddenObjects.showOnMiniMap and hiddenObjects.useSprites then
-			for i,sprite in pairs(hiddenObjects.sprites) do	hiddenObjects.sprites[i].sprite = GetSprite("hiddenObjects/"..sprite.spriteFile..".dds") end
+		if showOnMiniMap and useSprites then
+			for i,sprite in pairs(sprites) do	sprites[i].sprite = GetSprite("hiddenObjects/"..sprite.spriteFile..".dds") end
 		end
 		for i = 1, objManager.maxObjects do
 			local object = objManager:getObject(i)
