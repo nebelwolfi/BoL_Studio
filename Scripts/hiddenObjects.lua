94a7c63efa9b605baa26cfd0bf51f6a386cfb5e9
SurfaceS <surfaces@none>
2013-02-17 06:12:53 +0100

    added zyra, need check

--- Scripts/hiddenObjects.lua
+++ Scripts/hiddenObjects.lua
@@ -22,22 +22,23 @@
 do
 		--[[      CONFIG      ]]
 		showOnMiniMap = true			-- show objects on minimap
-		useSprites = false				-- show sprite on minimap
+		useSprites = true				-- show sprite on minimap
 		--[[      GLOBAL      ]]
 		objectsToAdd = {
-			{ name = "VisionWard", objectType = "wards", spellName = "VisionWard", charName = "VisionWard", color = 0x00FF00FF, range = 1450, duration = 180000, sprite = "yellowPoint"},
-			{ name = "SightWard", objectType = "wards", spellName = "SightWard", charName = "SightWard", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
-			{ name = "ItemGhostWard", objectType = "wards", spellName = "VisionWard", charName = "SightWard", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
-			{ name = "ItemMiniWard", objectType = "wards", spellName = "VisionWard", charName = "SightWard", color = 0x00FF00FF, range = 1450, duration = 60000, sprite = "greenPoint"},
-			{ name = "WriggleLantern", objectType = "wards", spellName = "WriggleLantern", charName = "WriggleLantern", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
-			{ name = "Jack In The Box", objectType = "boxes", spellName = "JackInTheBox", charName = "ShacoBox", color = 0x00FF0000, range = 300, duration = 60000, sprite = "redPoint"},
-			{ name = "Cupcake Trap", objectType = "traps", spellName = "CaitlynYordleTrap", charName = "CaitlynTrap", color = 0x00FF0000, range = 300, duration = 240000, sprite = "cyanPoint"},
-			{ name = "Noxious Trap", objectType = "traps", spellName = "Bushwhack", charName = "Nidalee_Spear", color = 0x00FF0000, range = 300, duration = 240000, sprite = "cyanPoint"},
-			{ name = "Noxious Trap", objectType = "traps", spellName = "BantamTrap", charName = "TeemoMushroom", color = 0x00FF0000, range = 300, duration = 600000, sprite = "cyanPoint"},
-			{ name = "Seed", objectType = "traps", spellName = "ZyraSeed", charName = "ZyraSeed", color = 0x00FF0000, range = 300, duration = 30000, sprite = "cyanPoint"},
+			{ name = "VisionWard", objectType = "wards", spellName = "visionward", charName = "VisionWard", color = 0x00FF00FF, range = 1450, duration = 180000, sprite = "yellowPoint"},
+			{ name = "SightWard", objectType = "wards", spellName = "sightward", charName = "SightWard", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
+			{ name = "VisionWard", objectType = "wards", spellName = "itemghostward", charName = "SightWard", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
+			{ name = "VisionWard", objectType = "wards", spellName = "itemminiward", charName = "SightWard", color = 0x00FF00FF, range = 1450, duration = 60000, sprite = "greenPoint"},
+			{ name = "SightWard", objectType = "wards", spellName = "wrigglelantern", charName = "SightWard", color = 0x0000FF00, range = 1450, duration = 180000, sprite = "greenPoint"},
+			{ name = "Jack In The Box", objectType = "boxes", spellName = "jackinthebox", charName = "ShacoBox", color = 0x00FF0000, range = 300, duration = 60000, sprite = "redPoint"},
+			{ name = "Cupcake Trap", objectType = "traps", spellName = "caitlynyordletrap", charName = "CaitlynTrap", color = 0x00FF0000, range = 300, duration = 240000, sprite = "cyanPoint"},
+			{ name = "Noxious Trap", objectType = "traps", spellName = "bushwhack", charName = "Nidalee_Spear", color = 0x00FF0000, range = 300, duration = 240000, sprite = "cyanPoint"},
+			{ name = "Noxious Trap", objectType = "traps", spellName = "bantamtrap", charName = "TeemoMushroom", color = 0x00FF0000, range = 300, duration = 600000, sprite = "cyanPoint"},
+			{ name = "Seed", objectType = "traps", spellName = "zyraseed", charName = "ZyraSeed", color = 0x00FF0000, range = 300, duration = 30000, sprite = "cyanPoint"},
 			-- to confirm spell
-			{ name = "DoABarrelRoll", objectType = "boxes", spellName = "MaokaiSapling2", charName = "MaokaiSproutling", color = 0x00FF0000, range = 300, duration = 35000, sprite = "redPoint"},
+			{ name = "DoABarrelRoll", objectType = "boxes", spellName = "maokaisapling2", charName = "MaokaiSproutling", color = 0x00FF0000, range = 300, duration = 35000, sprite = "redPoint"},
 		}
+		--
 		tmpObjects = {}
 		sprites = {
 			cyanPoint = { spriteFile = "PingMarkerCyan_8", }, 
@@ -49,29 +50,21 @@ do
 		objects = {}
 
 	--[[      CODE      ]]
-	function objectExist(spellName, pos, tick)
+	function objectExist(charName, pos, tick)
 		for i,obj in pairs(objects) do
-			if obj.object == nil and obj.spellName == spellName and GetDistance(obj.pos, pos) < 200 and tick < obj.seenTick then
+			if obj.object == nil and obj.charName == charName and GetDistance(obj.pos, pos) < 200 and tick < obj.seenTick then
 				return i
 			end
 		end	
 		return nil
 	end
-	
-	function removeObjectCasted(object, name)
-		for i,obj in pairs(objects) do
-			if obj.charName == name and GetDistance(obj.pos, object) < 200 then
-				objects[i] = nil
-			end
-		end	
-	end
-	
-	function addObject(objectToAdd, pos, fromSpell, object)
+
+	function addObject(objectToAdd, pos, object)
 		-- add the object
 		local tick = GetTickCount()
-		local objId = objectToAdd.spellName..(math.floor(pos.x) + math.floor(pos.z))
+		local objId = objectToAdd.charName..(math.floor(pos.x) + math.floor(pos.z))
 		--check if exist
-		local objectExist = objectExist(objectToAdd.spellName, {x = pos.x, z = pos.z,}, tick - 2000)
+		local objectExist = objectExist(objectToAdd.charName, {x = pos.x, z = pos.z,}, tick - 2000)
 		if objectExist ~= nil then
 			objId = objectExist
 		end
@@ -81,50 +74,39 @@ do
 				color = objectToAdd.color,
 				range = objectToAdd.range,
 				sprite = objectToAdd.sprite,
-				spellName = objectToAdd.spellName,
+				charName = objectToAdd.charName,
 				seenTick = tick,
 				endTick = tick + objectToAdd.duration,
-				fromSpell = fromSpell,
 				visible = (object == nil),
+				objectFound = (object ~= nil),
 				display = { visible = false, text = ""},
 			}
-			if (object ~= nil and object.mana ~= 0) then
-				objects[objId].endTick = tick + object.mana
-			end
 		elseif objects[objId].object == nil and object ~= nil and object.valid then
 			objects[objId].object = object
-			objects[objId].fromSpell = false
+			objects[objId].objectFound = true
 		end
+		if (object ~= nil and object.valid and object.maxMana > 0) then endTick = tick + object.mana end
 		objects[objId].pos = {x = pos.x, y = pos.y, z = pos.z, }
 		if showOnMiniMap == true then objects[objId].minimap = GetMinimap(pos) end
 	end
 
 	function OnCreateObj(object)
-		if object ~= nil and object.valid then
-			if object.type == "obj_AI_Minion" then
-				for i,objectToAdd in pairs(objectsToAdd) do
-					if object.name == objectToAdd.name and object.charName == objectToAdd.charName then
-						local tick = GetTickCount()
-						table.insert(tmpObjects, {tick = tick, object = object})
-					end
+		if object ~= nil and object.valid and object.type == "obj_AI_Minion" then
+			for i,objectToAdd in pairs(objectsToAdd) do
+				if object.name == objectToAdd.name then
+					local tick = GetTickCount()
+					table.insert(tmpObjects, {tick = tick, object = object})
 				end
-			elseif object.name == "ShroomMine.troy" then
-				removeObjectCasted(object, "TeemoMushroom")
-				--removeObjectCasted(object, "MaokaiSproutling")
-				--removeObjectCasted(object, "Nidalee_Spear")
-				--removeObjectCasted(object, "CaitlynTrap")
-				--removeObjectCasted(object, "ShacoBox")
-				--removeObjectCasted(object, "ZyraSeed")
 			end
 		end
 	end
 
 	function OnProcessSpell(object,spell)
-		if object ~= nil and object.valid and object.team == TEAM_ENEMY then
+		if object ~= nil and object.valid and object.team == TEAM_ENEMY and object.type == "obj_AI_Hero" then
 			for i,objectToAdd in pairs(objectsToAdd) do
-				if spell.name == objectToAdd.spellName then
+				if spell.name:lower() == objectToAdd.spellName then
 					ticked = GetTickCount()
-					addObject(objectToAdd, spell.endPos, true)
+					addObject(objectToAdd, spell.endPos)
 				end
 			end
 		end
@@ -151,7 +133,11 @@ do
 		local shiftKeyPressed = IsKeyDown(16)
 		for i,obj in pairs(objects) do
 			if obj.visible == true then
-				DrawCircle(obj.pos.x, obj.pos.y, obj.pos.z, 100, obj.color)
+				if obj.object ~= nil and obj.object.valid then
+					DrawCircle(obj.pos.x, obj.pos.y, obj.pos.z, 100, 0x00FFFFFF)
+				else
+					DrawCircle(obj.pos.x, obj.pos.y, obj.pos.z, 100, obj.color)
+				end
 				DrawCircle(obj.pos.x, obj.pos.y, obj.pos.z, (shiftKeyPressed and obj.range or 200), obj.color)
 				--minimap
 				if showOnMiniMap == true then
@@ -176,8 +162,8 @@ do
 				tmpObjects[i] = nil
 			else
 				for j,objectToAdd in pairs(objectsToAdd) do
-					if obj.object ~= nil and obj.object.valid and obj.object.charName == objectToAdd.charName and obj.object.team == TEAM_ENEMY then
-						addObject(objectToAdd, obj.object, false, obj.object)
+					if obj.object.charName == objectToAdd.charName and obj.object.team == TEAM_ENEMY then
+						addObject(objectToAdd, obj.object, obj.object)
 						tmpObjects[i] = nil
 						break
 					end
@@ -185,11 +171,13 @@ do
 			end
 		end
 		for i,obj in pairs(objects) do
-			if tick > obj.endTick or (obj.object ~= nil and obj.object.valid and obj.object.team == player.team) then
+			if tick > obj.endTick 
+			or (obj.objectFound and obj.object.valid and obj.object.team == player.team)
+			or (obj.objectFound and (not obj.object.valid or obj.object.dead or (obj.object.maxHealth > 0 and obj.object.health == 0))) then
 				objects[i] = nil
 			else
-				if obj.object == nil or obj.object.valid == false or (obj.object ~= nil and obj.object.valid and obj.object.dead ~= nil and obj.object.dead == false) then
-				--if obj.object == nil
+			--objectType = "wards"
+				if obj.object == nil or (obj.objectFound and obj.object.valid and not obj.object.dead) then
 					obj.visible = true
 				else
 					obj.visible = false
@@ -197,7 +185,7 @@ do
 				-- cursor pos
 				if obj.visible and GetDistanceFromMouse(obj.pos) < 150 then
 					local cursor = GetCursorPos()
-					obj.display.color = (obj.fromSpell and 0xFFFF0000 or 0xFF00FF00)
+					obj.display.color = (obj.objectFound and 0xFFFF0000 or 0xFF00FF00)
 					obj.display.text = timerText((obj.endTick-tick)/1000)
 					obj.display.x = cursor.x - 50
 					obj.display.y = cursor.y - 50
@@ -212,7 +200,7 @@ do
 	function OnLoad()
 		gameState = GameState()
 		if showOnMiniMap and useSprites then
-			for i,sprite in pairs(sprites) do	sprites[i].sprite = GetSprite("hiddenObjects/"..sprite.spriteFile..".dds") end
+			for i,sprite in pairs(sprites) do sprites[i].sprite = GetSprite("hiddenObjects/"..sprite.spriteFile..".dds") end
 		end
 		for i = 1, objManager.maxObjects do
 			local object = objManager:getObject(i)
