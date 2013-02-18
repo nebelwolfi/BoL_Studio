9f6fd0ea28644f291effbef7c869e700f2d07d2e
SurfaceS <surfaces@none>
2013-02-18 05:15:12 +0100

    reworked tower range

--- Scripts/towerRange.lua
+++ Scripts/towerRange.lua
@@ -1,91 +1,57 @@
 --[[
-	Script: Tower Range v0.2
+	Script: Tower Range v0.3
 	Author: SurfaceS
-
-	required libs : 		
-	required sprites : 		-
-	exposed variables : 	only libs variables
-
-	Usage : Press toggle key to walk thru :
-		-> 0 - Do nothing
-		-> 1 - Show enemy turrets range close to your champion (Default)
-		-> 2 - Show enemy turrets range
-		-> 3 - Show all turrets range
-		-> 4 - Show all turrets range close to your champion
 
 	v0.1 	initial release -- thanks Shoot for idea
 	V0.1b	added mode 4 -- thanks hellspan
 	v0.1c	added gameOver to stop script at end
 	v0.2	BoL Studio version
+	v0.3	use the scriptConfig
 ]]
 
 do
-	require "AllClass"
 	--[[         Globals        ]]
-	local towerRange = {
-		turrets = {},
-		typeText = {"OFF", "ON (enemy close)", "ON (enemy)", "ON (all)", "ON (all close)"},
-		--[[         Config         ]]
-		toggleKey = 116, 					-- F5
-		turretRange = 950,	 				-- 950
-		fountainRange = 1050,	 			-- 1050
-		allyTurretColor = 0x80FF00, 		-- Green color
-		enemyTurretColor = 0xFF0000, 		-- Red color
-		activeType = 1,						-- Default type (see usage)
-		tickUpdate = 1000,
-		nextUpdate = 0,
-	}
+	turrets = {}
+	--[[         Config         ]]
+	turretRange = 950	 				-- 950
+	fountainRange = 1050	 			-- 1050
+	visibilityRange = 1300	 			-- 1095
+	allyTurretColor = 0x064700	 		-- Green color
+	enemyTurretColor = 0xFF0000 		-- Red color
+	visibilityTurretColor = 0x470000 	-- Dark Red color
 
 	--[[         Code           ]]
-	function towerRange.checkTurretState()
-		if towerRange.activeType > 0 then
-			for name, turret in pairs(towerRange.turrets) do
-				turret.active = false
-			end
-			for i = 1, objManager.maxObjects do
-				local object = objManager:getObject(i)
-				if object ~= nil and object.type == "obj_AI_Turret" then
-					local name = object.name
-					if towerRange.turrets[name] ~= nil then towerRange.turrets[name].active = true end
-				end
-			end
-			for name, turret in pairs(towerRange.turrets) do
-				if turret.active == false then towerRange.turrets[name] = nil end
+	function checkTurretState()
+		for name, turret in pairs(turrets) do
+			if turret.object.valid == false then
+				turrets[name] = nil
 			end
 		end
 	end
 	
 	function OnDraw()
 		if gameState:gameIsOver() then return end
-		if towerRange.activeType > 0 then
-			for name, turret in pairs(towerRange.turrets) do
-				if turret ~= nil then
-					if (towerRange.activeType == 1 and turret.team ~= player.team and player.dead == false and GetDistance(turret) < 2000)
-					or (towerRange.activeType == 2 and turret.team ~= player.team)
-					or (towerRange.activeType == 3)
-					or (towerRange.activeType == 4 and player.dead == false and GetDistance(turret) < 2000) then
-						DrawCircle(turret.x, turret.y, turret.z, turret.range, turret.color)
+		for name, turret in pairs(turrets) do
+			if turret ~= nil then
+				if (TRConfig.showAlly or turret.team ~= player.team)
+				and (TRConfig.onlyClose == false or GetDistance(turret) < 2000) then
+					DrawCircle(turret.x, turret.y, turret.z, turret.range, turret.color)
+					if TRConfig.showVisibility then
+						DrawCircle(turret.x, turret.y, turret.z, visibilityRange, visibilityTurretColor)
 					end
 				end
 			end
 		end
 	end
 	function OnTick()
-		if IsKeyPressed(towerRange.toggleKey) then
-			towerRange.activeType = (towerRange.activeType < 4 and towerRange.activeType + 1 or 0)
-			PrintChat("Turret range display is "..towerRange.typeText[towerRange.activeType + 1])
-		end
-		local tick = GetTickCount()
-		if tick > towerRange.nextUpdate then
-			towerRange.nextUpdate = tick + towerRange.tickUpdate
-			towerRange.checkTurretState()
-		end
+		if gameState:gameIsOver() then return end
+		checkTurretState()
 	end
 	function OnDeleteObj(object)
 		if object ~= nil and object.type == "obj_AI_Turret" then
-			for name, turret in pairs(towerRange.turrets) do
+			for name, turret in pairs(turrets) do
 				if name == object.name then
-					towerRange.turrets[name] = nil
+					turrets[name] = nil
 					return
 				end
 			end
@@ -93,29 +59,33 @@ do
 	end
 	function OnLoad()
 		gameState = GameState()
+		TRConfig = scriptConfig("Tower range 0.3", "towerRange")
+		TRConfig:addParam("onlyClose", "Show only close", SCRIPT_PARAM_ONOFF, true)
+		TRConfig:addParam("showAlly", "Show ally turrets", SCRIPT_PARAM_ONOFF, false)
+		TRConfig:addParam("showVisibility", "Show turret view", SCRIPT_PARAM_ONOFF, true)
 		for i = 1, objManager.maxObjects do
 			local object = objManager:getObject(i)
 			if object ~= nil and object.type == "obj_AI_Turret" then
 				local turretName = object.name
-				towerRange.turrets[turretName] = {
+				turrets[turretName] = {
 					object = object,
 					team = object.team,
-					color = (object.team == player.team and towerRange.allyTurretColor or towerRange.enemyTurretColor),
-					range = towerRange.turretRange,
+					color = (object.team == player.team and allyTurretColor or enemyTurretColor),
+					range = turretRange,
 					x = object.x,
 					y = object.y,
 					z = object.z,
 					active = false,
 				}
 				if turretName == "Turret_OrderTurretShrine_A" or turretName == "Turret_ChaosTurretShrine_A" then
-					towerRange.turrets[turretName].range = towerRange.fountainRange
+					turrets[turretName].range = fountainRange
 					for j = 1, objManager.maxObjects do
 						local object2 = objManager:getObject(j)
 						if object2 ~= nil and object2.type == "obj_SpawnPoint" and GetDistance(object, object2) < 1000 then
-							towerRange.turrets[turretName].x = object2.x
-							towerRange.turrets[turretName].z = object2.z
+							turrets[turretName].x = object2.x
+							turrets[turretName].z = object2.z
 						elseif object2 ~= nil and object2.type == "obj_HQ" and object2.team == object.team then
-							towerRange.turrets[turretName].y = object2.y
+							turrets[turretName].y = object2.y
 						end
 					end
 				end
