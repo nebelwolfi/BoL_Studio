0aee13d65fe731fc4b458b03fd7000df9bfeb6e0
SurfaceS <surfaces@none>
2013-02-18 02:16:06 +0100

    full reworked

--- Scripts/minimapTimers.lua
+++ Scripts/minimapTimers.lua
@@ -7,17 +7,18 @@
 		UPDATES :
 		v0.1					initial release
 ]]
-require "AllClass"
 do
 	--[[      GLOBAL      ]]
 	monsters = {
 		summonerRift = {
 			{	-- baron
 				name = "baron",
+				spawn = 900,
 				respawn = 420,
 				advise = true,
 				camps = {
 					{
+						pos = { x = 4600, y = 60, z = 10250 },
 						name = "monsterCamp_12",
 						creeps = { { { name = "Worm12.1.1" }, }, },
 						team = TEAM_NEUTRAL,
@@ -26,10 +27,12 @@ do
 			},
 			{	-- dragon
 				name = "dragon",
+				spawn = 150,
 				respawn = 360,
 				advise = true,
 				camps = {
 					{
+						pos = { x = 9459, y = 60, z = 4193 },
 						name = "monsterCamp_6",
 						creeps = { { { name = "Dragon6.1.1" }, }, },
 						team = TEAM_NEUTRAL,
@@ -38,15 +41,18 @@ do
 			},
 			{	-- blue
 				name = "blue",
+				spawn = 115,
 				respawn = 300,
 				advise = true,
 				camps = {
 					{
+						pos = { x = 3632, y = 60, z = 7600 },
 						name = "monsterCamp_1",
 						creeps = { { { name = "AncientGolem1.1.1" }, { name = "YoungLizard1.1.2" }, { name = "YoungLizard1.1.3" }, }, },
 						team = TEAM_BLUE,
 					},
 					{
+						pos = { x = 10386, y = 60, z = 6811 },
 						name = "monsterCamp_7",
 						creeps = { { { name = "AncientGolem7.1.1" }, { name = "YoungLizard7.1.2" }, { name = "YoungLizard7.1.3" }, }, },
 						team = TEAM_RED,
@@ -55,15 +61,18 @@ do
 			},
 			{	-- red
 				name = "red",
+				spawn = 115,
 				respawn = 300,
 				advise = true,
 				camps = {
 					{
+						pos = { x = 7455, y = 60, z = 3890 },
 						name = "monsterCamp_4",
 						creeps = { { { name = "LizardElder4.1.1" }, { name = "YoungLizard4.1.2" }, { name = "YoungLizard4.1.3" }, }, },
 						team = TEAM_BLUE,
 					},
 					{
+						pos = { x = 6504, y = 60, z = 10584 },
 						name = "monsterCamp_10",
 						creeps = { { { name = "LizardElder10.1.1" }, { name = "YoungLizard10.1.2" }, { name = "YoungLizard10.1.3" }, }, },
 						team = TEAM_RED,
@@ -72,6 +81,7 @@ do
 			},
 			{	-- wolves
 				name = "wolves",
+				spawn = 100,
 				respawn = 60,
 				advise = false,
 				camps = {
@@ -89,6 +99,7 @@ do
 			},
 			{	-- wraiths
 				name = "wraiths",
+				spawn = 100,
 				respawn = 50,
 				advise = false,
 				camps = {
@@ -106,6 +117,7 @@ do
 			},
 			{	-- Golems
 				name = "Golems",
+				spawn = 100,
 				respawn = 60,
 				advise = false,
 				camps = {
@@ -125,10 +137,12 @@ do
 		twistedTreeline = {
 			{	-- Wraith
 				name = "Wraith",
+				spawn = 100,
 				respawn = 50,
 				advise = false,
 				camps = {
 					{
+						--pos = { x = 4414, y = 60, z = 5774 },
 						name = "monsterCamp_1",
 						creeps = {
 							{ { name = "TT_NWraith1.1.1" }, { name = "TT_NWraith21.1.2" }, { name = "TT_NWraith21.1.3" }, },
@@ -136,6 +150,7 @@ do
 						team = TEAM_BLUE,
 					},
 					{
+						--pos = { x = 11008, y = 60, z = 5775 },
 						name = "monsterCamp_4",
 						creeps = {
 							{ { name = "TT_NWraith4.1.1" }, { name = "TT_NWraith24.1.2" }, { name = "TT_NWraith24.1.3" }, },
@@ -147,9 +162,11 @@ do
 			{	-- Golems
 				name = "Golems",
 				respawn = 50,
+				spawn = 100,
 				advise = false,
 				camps = {
 					{
+						--pos = { x = 5088, y = 60, z = 8065 },
 						name = "monsterCamp_2",
 						creeps = {
 							{ { name = "TT_NGolem2.1.1" }, { name = "TT_NGolem22.1.2" } },
@@ -157,6 +174,7 @@ do
 						team = TEAM_BLUE,
 					},
 					{
+						--pos = { x = 10341, y = 60, z = 8084 },
 						name = "monsterCamp_5",
 						creeps = {
 							{ { name = "TT_NGolem5.1.1" }, { name = "TT_NGolem25.1.2" } },
@@ -168,14 +186,17 @@ do
 			{	-- Wolves
 				name = "Wolves",
 				respawn = 50,
+				spawn = 100,
 				advise = false,
 				camps = {
 					{
+						--pos = { x = 6148, y = 60, z = 5993 },
 						name = "monsterCamp_3",
 						creeps = { { { name = "TT_NWolf3.1.1" }, { name = "TT_NWolf23.1.2" }, { name = "TT_NWolf23.1.3" } }, },
 						team = TEAM_BLUE,
 					},
 					{
+						--pos = { x = 9239, y = 60, z = 6022 },
 						name = "monsterCamp_6",
 						creeps = { { { name = "TT_NWolf6.1.1" }, { name = "TT_NWolf26.1.2" }, { name = "TT_NWolf26.1.3" } }, },
 						team = TEAM_RED,
@@ -184,10 +205,12 @@ do
 			},
 			{	-- Heal
 				name = "Heal",
+				spawn = 115,
 				respawn = 90,
 				advise = true,
 				camps = {
 					{
+						pos = { x = 7711, y = 60, z = 6722 },
 						name = "monsterCamp_7",
 						creeps = { { { name = "TT_Relic7.1.1" }, }, },
 						team = TEAM_NEUTRAL,
@@ -196,10 +219,12 @@ do
 			},
 			{	-- Vilemaw
 				name = "Vilemaw",
+				spawn = 600,
 				respawn = 300,
 				advise = true,
 				camps = {
 					{
+						pos = { x = 7711, y = 60, z = 10080 },
 						name = "monsterCamp_8",
 						creeps = { { { name = "TT_Spiderboss8.1.1" }, }, },
 						team = TEAM_NEUTRAL,
@@ -207,9 +232,100 @@ do
 				},
 			},
 		},
+		crystalScar = {},
+		provingGrounds = {
+			{	-- Heal
+				name = "Heal",
+				spawn = 190,
+				respawn = 40,
+				advise = false,
+				camps = {
+					{
+						pos = { x = 8922, y = 60, z = 7868 },
+						name = "monsterCamp_1",
+						creeps = { { { name = "OdinShieldRelic1.1.1" }, }, },
+						team = TEAM_NEUTRAL,
+					},
+					{
+						pos = { x = 7473, y = 60, z = 6617 },
+						name = "monsterCamp_2",
+						creeps = { { { name = "OdinShieldRelic2.1.1" }, }, },
+						team = TEAM_NEUTRAL,
+					},
+					{
+						pos = { x = 5929, y = 60, z = 5190 },
+						name = "monsterCamp_3",
+						creeps = { { { name = "OdinShieldRelic3.1.1" }, }, },
+						team = TEAM_NEUTRAL,
+					},
+					{
+						pos = { x = 4751, y = 60, z = 3901 },
+						name = "monsterCamp_4",
+						creeps = { { { name = "OdinShieldRelic4.1.1" }, }, },
+						team = TEAM_NEUTRAL,
+					},
+				},
+			},
+		}
 	}
 	
-	function addCampAndCreep(object)
+	altars = {
+		summonerRift = {},
+		twistedTreeline = {
+			{
+				name = "Left Altar",
+				spawn = 180,
+				respawn = 90,
+				advise = true,
+				objectName = "TT_Buffplat_L",
+				locked = false,
+				lockNames = {"TT_Lock_Blue_L.troy", "TT_Lock_Red_L.troy", "TT_Lock_Neutral_L.troy", },
+				unlockNames = {"TT_Unlock_Blue_L.troy", "TT_Unlock_Red_L.troy", "TT_Unlock_Neutral_L.troy", },
+			},
+			{
+				name = "Right Altar",
+				spawn = 180,
+				respawn = 90,
+				advise = true,
+				objectName = "TT_Buffplat_R",
+				locked = false,
+				lockNames = {"TT_Lock_Blue_R.troy", "TT_Lock_Red_R.troy", "TT_Lock_Neutral_R.troy", },
+				unlockNames = {"TT_Unlock_Blue_R.troy", "TT_Unlock_Red_R.troy", "TT_Unlock_Neutral_R.troy", },
+			},
+		},
+		crystalScar = {},
+		provingGrounds = {},
+	}
+	
+	relics = {
+		summonerRift = {},
+		twistedTreeline = {},
+		crystalScar = {
+			{
+				name = "Relic",
+				team = TEAM_BLUE,
+				spawn = 180,
+				respawn = 180,
+				advise = true,
+				objectName = "Info_LargeRelic1",
+				locked = false,
+				precenceObject = "Odin_Prism_Green.troy",
+			},
+			{
+				name = "Relic",
+				team = TEAM_RED,
+				spawn = 180,
+				respawn = 180,
+				advise = true,
+				objectName = "Info_LargeRelic2",
+				locked = false,
+				precenceObject = "Odin_Prism_Red.troy",
+			},
+		},
+		provingGrounds = {},
+	}
+
+	function addCampCreepAltar(object)
 		if object ~= nil and object.name ~= nil then
 			for i,monster in pairs(monsters[mapName]) do
 				for j,camp in pairs(monster.camps) do
@@ -227,6 +343,44 @@ do
 							end
 						end
 					end
+				end
+			end
+			for i,altar in pairs(altars[mapName]) do
+				if altar.objectName == object.name then
+					altar.object = object
+					altar.minimap = GetMinimap(object)
+				end
+				if altar.locked then
+					for j,lockName in pairs(altar.unlockNames) do
+						if lockName == object.name then
+							altar.locked = false
+							return
+						end
+					end
+				else
+					for j,lockName in pairs(altar.lockNames) do
+						if lockName == object.name then
+							altar.drawColor = 0
+							altar.drawText = ""
+							altar.locked = true
+							altar.advised = false
+							altar.advisedBefore = false
+							return
+						end
+					end
+				end
+			end
+			for i,relic in pairs(relics[mapName]) do
+				if relic.objectName == object.name then
+					relic.founded = true
+					relic.minimap = GetMinimap(object)
+					return
+				elseif relic.precenceObject == object.name then
+					relic.object = object
+					relic.locked = false
+					relic.advisedBefore = false
+					relic.advised = false
+					return
 				end
 			end
 		end
@@ -254,8 +408,9 @@ do
 		if monsters[mapName] == nil then
 			mapName = nil
 			monsters = nil
-			addCampAndCreep = nil
+			addCampCreepAltar = nil
 			removeCreep = nil
+			addAltarObject = nil
 			return
 		else
 			startTick = GetStart().tick
@@ -280,12 +435,12 @@ do
 			for i = 1, objManager.maxObjects do
 				local object = objManager:getObject(i)
 				if object ~= nil then 
-					addCampAndCreep(object)
+					addCampCreepAltar(object)
 				end
 			end
-			AddCreateObjCallback(addCampAndCreep)
+			AddCreateObjCallback(addCampCreepAltar)
 			AddDeleteObjCallback(removeCreep)
-
+			
 			function OnTick()
 				if gameState:gameIsOver() then return end
 				local GameTime = (GetTickCount()-startTick) / 1000
@@ -322,7 +477,6 @@ do
 							if campStatus == 0 then
 								if (camp.status == 1 or camp.status == 2) then
 									campStatus = 4
-									camp.deathTime = GameTime
 									camp.advisedBefore = false
 									camp.advised = false
 									camp.respawnTime = math.ceil(GameTime) + monster.respawn
@@ -332,6 +486,15 @@ do
 								else
 									campStatus = 3
 								end
+							end
+						elseif camp.pos ~= nil then
+							camp.minimap = GetMinimap(camp.pos)
+							if (GameTime < monster.spawn) then
+								campStatus = 4
+								camp.advisedBefore = true
+								camp.advised = true
+								camp.respawnTime = monster.spawn
+								camp.respawnText = (camp.enemyTeam and "Enemy " or "")..monster.name.." spawn at "..TimerText(camp.respawnTime)
 							end
 						end
 						if camp.status ~= campStatus or campStatus == 4 then
@@ -347,17 +510,17 @@ do
 								camp.drawColor = 0xFFFF0000
 							elseif camp.status == 3 then			-- ready, not creeps shown
 								camp.drawText = "   ?"
-								camp.drawColor = 0xFF00FF00			
+								camp.drawColor = 0xFF00FF00
 							elseif camp.status == 4 then			-- empty from creeps kill
 								local secondLeft = math.ceil(math.max(0, camp.respawnTime - GameTime))
 								if monster.advise == true and (MMTConfig.adviceEnemyMonsters == true or camp.enemyTeam == false) then
 									if secondLeft == 0 and camp.advised == false then
 										camp.advised = true
-										if MMTConfig.textOnRespawn then PrintChat("<font color='#00FFCC'>"..(camp.enemyTeam and "Enemy " or "")..monster.name.."</font> has respawned") end
+										if MMTConfig.textOnRespawn then PrintChat("<font color='#00FFCC'>"..(camp.enemyTeam and "Enemy " or "")..monster.name.."</font><font color='#FFAA00'> has respawned</font>") end
 										if MMTConfig.pingOnRespawn then PingSignal(PING_FALLBACK,camp.object.x,camp.object.y,camp.object.z,2) end
 									elseif secondLeft <= MMTConfig.adviceBefore and camp.advisedBefore == false then
 										camp.advisedBefore = true
-										if MMTConfig.textOnRespawnBefore then PrintChat("<font color='#00FFCC'>"..(camp.enemyTeam and "Enemy " or "")..monster.name.."</font> will respawn in "..secondLeft.." sec") end
+										if MMTConfig.textOnRespawnBefore then PrintChat("<font color='#00FFCC'>"..(camp.enemyTeam and "Enemy " or "")..monster.name.."</font><font color='#FFAA00'> will respawn in </font><font color='#00FFCC'>"..secondLeft.." sec</font>") end
 										if MMTConfig.pingOnRespawnBefore then PingSignal(PING_FALLBACK,camp.object.x,camp.object.y,camp.object.z,2) end
 									end
 								end
@@ -381,6 +544,81 @@ do
 						end
 					end
 				end
+			
+				-- altars
+				for i,altar in pairs(altars[mapName]) do
+					if altar.object and altar.object.valid then
+						if altar.locked then
+							local tmpTime = ((altar.object.maxMana - altar.object.mana) / 20100)
+							if GameTime < altar.spawn then
+								altar.secondLeft = math.ceil(math.max(0, altar.spawn - GameTime))
+							else
+								altar.secondLeft = math.ceil(math.max(0, altar.respawn - (tmpTime * altar.respawn)))
+							end
+							altar.unlockTime = math.ceil(GameTime + altar.secondLeft)
+							altar.unlockText = altar.name.." unlock at "..TimerText(altar.unlockTime)
+							altar.drawColor = 0xFFFFFF00
+							if altar.advise == true then
+								if altar.secondLeft == 0 and altar.advised == false then
+									altar.advised = true
+									if MMTConfig.textOnRespawn then PrintChat("<font color='#00FFCC'>"..altar.name.."</font><font color='#FFAA00'> is unlocked</font>") end
+									if MMTConfig.pingOnRespawn then PingSignal(PING_FALLBACK,altar.object.x,altar.object.y,altar.object.z,2) end
+								elseif altar.secondLeft <= MMTConfig.adviceBefore and altar.advisedBefore == false then
+									altar.advisedBefore = true
+									if MMTConfig.textOnRespawnBefore then PrintChat("<font color='#00FFCC'>"..altar.name.."</font><font color='#FFAA00'> will unlock in </font><font color='#00FFCC'>"..altar.secondLeft.." sec</font>") end
+									if MMTConfig.pingOnRespawnBefore then PingSignal(PING_FALLBACK,altar.object.x,altar.object.y,altar.object.z,2) end
+								end
+							end
+							-- shift click
+							if IsKeyDown(16) then
+								altar.drawText = " "..(altar.unlockTime ~= nil and TimerText(altar.unlockTime) or "")
+								altar.textUnder = (CursorIsUnder(altar.minimap.x - 9, altar.minimap.y - 5, 20, 8))
+							else
+								altar.drawText = " "..(altar.secondLeft ~= nil and TimerText(altar.secondLeft) or "")
+								altar.textUnder = false
+							end
+						end
+					end
+				end
+			
+				-- relics
+				for i,relic in pairs(relics[mapName]) do
+					if relic.founded then
+						if (not relic.locked and not relic.object or not relic.object.valid) then
+							if GameTime < relic.spawn then
+								relic.unlockTime = relic.spawn
+							else
+								relic.unlockTime = math.ceil(GameTime + relic.respawn)
+							end
+							relic.unlockText = relic.name.." respawn at "..TimerText(relic.unlockTime)							
+							relic.locked = true
+							relic.drawColor = 0xFFFFFF00
+						end
+						if relic.locked then
+							relic.secondLeft = math.ceil(math.max(0, relic.unlockTime - GameTime))
+							relic.drawText = " "..(relic.secondLeft ~= nil and TimerText(relic.secondLeft) or "")
+							if relic.advise == true then
+								if relic.secondLeft == 0 and relic.advised == false then
+									relic.advised = true
+									if MMTConfig.textOnRespawn then PrintChat("<font color='#00FFCC'>"..relic.name.."</font><font color='#FFAA00'> has respawned</font>") end
+									if MMTConfig.pingOnRespawn then PingSignal(PING_FALLBACK,relic.object.x,relic.object.y,relic.object.z,2) end
+								elseif relic.secondLeft <= MMTConfig.adviceBefore and relic.advisedBefore == false then
+									relic.advisedBefore = true
+									if MMTConfig.textOnRespawnBefore then PrintChat("<font color='#00FFCC'>"..relic.name.."</font><font color='#FFAA00'> will respawn in </font><font color='#00FFCC'>"..relic.secondLeft.." sec</font>") end
+									if MMTConfig.pingOnRespawnBefore then PingSignal(PING_FALLBACK,relic.object.x,relic.object.y,relic.object.z,2) end
+								end
+							end
+							-- shift click
+							if IsKeyDown(16) then
+								relic.drawText = " "..(relic.unlockTime ~= nil and TimerText(relic.unlockTime) or "")
+								relic.textUnder = (CursorIsUnder(relic.minimap.x - 9, relic.minimap.y - 5, 20, 8))
+							else
+								relic.drawText = " "..(relic.secondLeft ~= nil and TimerText(relic.secondLeft) or "")
+								relic.textUnder = false
+							end
+						end
+					end
+				end
 			end
 
 			function OnDraw()
@@ -394,6 +632,16 @@ do
 								DrawText(camp.drawText,16,camp.minimap.x - 9, camp.minimap.y - 5, camp.drawColor)
 							end
 						end
+					end
+				end
+				for i,altar in pairs(altars[mapName]) do
+					if altar.locked then
+						DrawText(altar.drawText,16,altar.minimap.x - 9, altar.minimap.y - 5, altar.drawColor)
+					end
+				end
+				for i,relic in pairs(relics[mapName]) do
+					if relic.founded and relic.locked then
+						DrawText(relic.drawText,16,relic.minimap.x - 9, relic.minimap.y - 5, relic.drawColor)
 					end
 				end
 			end
@@ -413,6 +661,12 @@ do
 									end
 								end
 							end
+						end
+					end
+					for i,altar in pairs(altars[mapName]) do
+						if altar.locked and altar.textUnder then
+							if altar.unlockText ~= nil then SendChat(""..altar.unlockText) end
+							break
 						end
 					end
 				end
