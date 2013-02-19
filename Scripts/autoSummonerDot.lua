5f93d6643593a1989db05f9b532ffb0947110e86
SurfaceS <surfaces@none>
2013-02-19 19:26:46 +0100

    SummonerExhaust

--- Scripts/autoSummonerDot.lua
+++ Scripts/autoSummonerDot.lua
@@ -1,112 +1,82 @@
 --[[
-	Script: Auto SummonerDot v0.2
+	Script: Auto SummonerDot v0.3
 	Author: SurfaceS
-
-	required libs : 		-
-	required sprites : 		-
-	exposed variables : 	player
-	
 	v0.1 	initial release
 	v0.2	BoL Studio Version
+	v0.3	Reworked
 ]]
 
 --[[ 		Globals		]]
-require "AllClass"
 do
-	local autoSummonerDot = {
-		range = 600,
-		baseDamage = 50,
-		damagePerLevel = 20,
-		castDelay = 0,
-		haveDisplay = false,
-		activeKey = 32,			-- Press key to use autoSummonerDot mode (space by default)
-		toggleKey = 114,		-- Press key to toggle autoSummonerDot mode (F3/F4 by default -> slot)
-		forceIgniteKey = 84,	-- Press key to force ignite (tTt by default)
-		active = false,
-		toggled = false,
-		forced = false,
-		forcedTick = 0,
-	}
---[[ 		Code		]]
 	function OnLoad()
-		--[[            Conditional            ]]
-		if player == nil then player = GetMyHero() end
 		if string.find(player:GetSpellData(SUMMONER_1).name..player:GetSpellData(SUMMONER_2).name, "SummonerDot") ~= nil then
+			local key = 114
 			if player:GetSpellData(SUMMONER_1).name == "SummonerDot" then
-				autoSummonerDot.slot = SUMMONER_1
+				slot = SUMMONER_1
 			elseif player:GetSpellData(SUMMONER_2).name == "SummonerDot" then
-				autoSummonerDot.slot = SUMMONER_2
-				autoSummonerDot.toggleKey = autoSummonerDot.toggleKey + 1
+				slot = SUMMONER_2
+				key = 115
 			end
-			function autoSummonerDot.ready()
-				if autoSummonerDot.slot ~= nil and autoSummonerDot.castDelay < GetTickCount() and player:CanUseSpell(autoSummonerDot.slot) == READY then return true end
-				return false
-			end
-			function autoSummonerDot.autoIgniteIfKill()
-				if autoSummonerDot.ready() then
-					local damage = autoSummonerDot.baseDamage + autoSummonerDot.damagePerLevel * player.level
+			myConfig = scriptConfig("Summoner Heal", "summonerHeal")
+			myConfig:addParam("ADotOnKey", "Auto Ignite key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+			myConfig:addParam("ADot", "Perma Auto Ignite", SCRIPT_PARAM_ONKEYTOGGLE, true, key)
+			myConfig:addParam("forcedDot", "Forced Ignite", SCRIPT_PARAM_ONKEYDOWN, false, 84)  --(tTt by default)
+			castDelay = 0
+			damagePerLevel = 20
+			baseDamage = 50
+			range = 600
+			forced = false
+			forcedTick = 0
+			
+			function autoIgniteIfKill()
+				if slot ~= nil and castDelay < GetTickCount() and player:CanUseSpell(slot) == READY then
+					local damage = baseDamage + damagePerLevel * player.level
 					for i = 1, heroManager.iCount, 1 do
 						local hero = heroManager:getHero(i)
-						if hero ~= nil and hero.team ~= player.team and not hero.dead and hero.visible and player:GetDistance(hero) < autoSummonerDot.range and hero.health <= damage then
-							return autoSummonerDot.igniteTarget( hero )
+						if ValidTarget(hero, range) and hero.health <= damage then
+							return igniteTarget( hero )
 						end
 					end
 				end
-				return nil
 			end
-			function autoSummonerDot.autoIgniteLowestHealth()
-				if autoSummonerDot.ready() then
+			function autoIgniteLowestHealth()
+				if slot ~= nil and castDelay < GetTickCount() and player:CanUseSpell(slot) == READY then
 					local minLifeHero = nil
 					for i = 1, heroManager.iCount, 1 do
 						local hero = heroManager:getHero(i)
-						if hero ~= nil and hero.team ~= player.team and not hero.dead and hero.visible and player:GetDistance(hero) <= autoSummonerDot.range then
+						if ValidTarget(hero, range) then
 							if minLifeHero == nil or hero.health < minLifeHero.health then
 								minLifeHero = hero
 							end
 						end
 					end
 					if minLifeHero ~= nil then
-						return autoSummonerDot.igniteTarget( minLifeHero )
+						return igniteTarget( minLifeHero )
 					end
 				end
-				return nil
 			end
-			function autoSummonerDot.igniteTarget(target)
-				if autoSummonerDot.ready() then
-					CastSpell(autoSummonerDot.slot, target)
-					autoSummonerDot.castDelay = GetTickCount() + 500
+			function igniteTarget(target)
+				if slot ~= nil and castDelay < GetTickCount() and player:CanUseSpell(slot) == READY then
+					CastSpell(slot, target)
+					castDelay = GetTickCount() + 500
 					return target
-				end
-				return nil
-			end
-			function OnWndMsg(msg,wParam)
-				if msg == KEY_DOWN then
-					if wParam == autoSummonerDot.forceIgniteKey and autoSummonerDot.ready() then
-						if autoSummonerDot.forced == false and autoSummonerDot.haveDisplay == false then PrintChat(" >> Auto ignite forced") end
-						autoSummonerDot.forced = true
-						autoSummonerDot.forcedTick = GetTickCount() + 1000
-					elseif wParam == autoSummonerDot.toggleKey then
-						autoSummonerDot.toggled = not autoSummonerDot.toggled
-						autoSummonerDot.active = autoSummonerDot.toggled
-						if autoSummonerDot.haveDisplay == false then PrintChat(" >> Auto ignite : "..(autoSummonerDot.active and "ON" or "OFF")) end
-					end
 				end
 			end
 			function OnTick()
 				local tick = GetTickCount()
-				if autoSummonerDot.toggled == false then autoSummonerDot.active = IsKeyDown(autoSummonerDot.activeKey) end
-				if autoSummonerDot.forced then
-					if autoSummonerDot.forcedTick > tick then
-						if autoSummonerDot.autoIgniteLowestHealth() ~= nil then autoSummonerDot.forced = false end
-					else
-						autoSummonerDot.forced = false
+				if myConfig.forcedDot and (slot ~= nil and player:CanUseSpell(slot) == READY) then
+					forced = true
+					forcedTick = GetTickCount() + 2000
+				end
+				if forced then
+					PrintFloatText(player,0,"Auto ignite forced")
+					if autoIgniteLowestHealth() ~= nil or forcedTick < tick then
+						forced = false
 					end
-				elseif autoSummonerDot.active then
-					autoSummonerDot.autoIgniteIfKill()
+				elseif myConfig.ADot or myConfig.ADotOnKey then
+					autoIgniteIfKill()
 				end
 			end
-		else
-			autoSummonerDot = nil
 		end
 	end
 end
