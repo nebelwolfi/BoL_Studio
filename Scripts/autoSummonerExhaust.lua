da0b79ed6faf7a927d987273c7573ddaae2ddb91
SurfaceS <surfaces@none>
2013-02-19 18:55:42 +0100

    updated summoner exaust

--- Scripts/autoSummonerExhaust.lua
+++ Scripts/autoSummonerExhaust.lua
@@ -1,68 +1,52 @@
 --[[
-	Script: Auto SummonerExhaust v0.2
+	Script: Auto SummonerExhaust v0.3
 	Author: SurfaceS
 
-	required libs : 		-
-	required sprites : 		-
-	exposed variables : 	player
-	
 	v0.1 	initial release
 	v0.2	BoL Studio Version
+	v0.3	added menu
 ]]
-require "AllClass"
+
 do
-	--[[ 		Globals		]]
-	local autoSummonerExhaust = {
-		castDelay = 0,
-		active = false,
-		activeTick = 0,
-		range = 550,
-		haveDisplay = false,		-- don't display chat
-		activeKey = 84,			-- Press key to use autoSummonerExhaust mode (tTt by default)
-	}
---[[ 		Code		]]
 	function OnLoad()
-		--[[            Conditional            ]]
 		if string.find(player:GetSpellData(SUMMONER_1).name..player:GetSpellData(SUMMONER_2).name, "SummonerExhaust") ~= nil then
+			active = false
+			activeTick = 0
+			range = 550
+			myConfig = scriptConfig("Exhaust Config", "summonerExhaust")
+			myConfig:addParam("activeKey", "Activate key", SCRIPT_PARAM_ONKEYDOWN, false, 84) --(tTt by default)
 			if player:GetSpellData(SUMMONER_1).name == "SummonerExhaust" then
-				autoSummonerExhaust.slot = SUMMONER_1
+				slot = SUMMONER_1
 			elseif player:GetSpellData(SUMMONER_2).name == "SummonerExhaust" then
-				autoSummonerExhaust.slot = SUMMONER_2
-			end
-			function autoSummonerExhaust.ready()
-				return autoSummonerExhaust.slot ~= nil and autoSummonerExhaust.castDelay < GetTickCount() and player:CanUseSpell(autoSummonerExhaust.slot) == READY
-			end
-			function OnWndMsg(msg,wParam)
-				if msg == KEY_DOWN and wParam == autoSummonerExhaust.activeKey and autoSummonerExhaust.ready() then
-					if autoSummonerExhaust.active == false and autoSummonerExhaust.haveDisplay == false then PrintChat(" >> Auto exhaust forced") end
-					autoSummonerExhaust.active = true
-					autoSummonerExhaust.activeTick = GetTickCount() + 1000
-				end
+				slot = SUMMONER_2
 			end
 			function OnTick()
-				local tick = GetTickCount()
-				if autoSummonerExhaust.active then
-					if autoSummonerExhaust.ready() then
+				if myConfig.activeKey and (slot ~= nil and player:CanUseSpell(slot) == READY) then
+					if active == false then
+						active = true
+					end
+					activeTick = GetTickCount() + 2000
+				end
+				if active then
+					PrintFloatText(player,0,"Auto exhaust forced")
+					if (slot ~= nil and player:CanUseSpell(slot) == READY) then
 						local maxDPShero = nil
 						local maxDPS = 0
 						for i = 1, heroManager.iCount, 1 do
 							local hero = heroManager:getHero(i)
-							if ValidTarget(hero,autoSummonerExhaust.range + 100) then
+							if ValidTarget(hero,range + 200) then
 								local dps = hero.totalDamage * hero.attackSpeed
 								if maxDPShero == nil or maxDPS < dps then maxDPS, maxDPShero = dps, hero end
 							end
 						end
 						if maxDPShero ~= nil then
-							autoSummonerExhaust.active = false
-							autoSummonerExhaust.castDelay = tick + 500
-							CastSpell(autoSummonerExhaust.slot, maxDPShero)
+							active = false
+							CastSpell(slot, maxDPShero)
 						end
 					end
-					if autoSummonerExhaust.activeTick < tick then autoSummonerExhaust.active = false end
+					if activeTick < GetTickCount() then active = false end
 				end
 			end
-		else
-			autoSummonerExhaust = nil
 		end
 	end
 end
