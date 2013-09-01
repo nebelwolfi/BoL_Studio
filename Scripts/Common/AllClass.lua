55f156b639554ab5161919c8463757a771541bc3
mrgreywater <mr.greywater@googlemail.com>
2013-09-01 21:33:45 +0200

    updated GetWebSprite

--- Scripts/Common/AllClass.lua
+++ Scripts/Common/AllClass.lua
@@ -907,23 +907,25 @@ function GetSprite(file, altFile)
     end
 end
 
---[[
-    GetWebSprite(url, [callback])
-    returns a sprite from a given website
-    if no callback is given, it returns it result immediatly, if a callback is given, it downloads the sprite asyncronly and returns the sprite in the callback (recommended).
-]]
-function GetWebSprite(url, callback)
+--[=[
+    GetWebSprite(url, [callback, [path]])
+returns a sprite from a given website
+if no callback is given, it returns it result immediately, if a callback is given, it downloads the sprite asynchrony and returns the sprite in the callback (recommended).
+]=]
+function GetWebSprite(url, callback, path)
     local urlr, sprite = url:reverse(), nil
     local filename, env = urlr:sub(1, urlr:find("/") - 1):reverse(), GetCurrentEnv() and GetCurrentEnv().FILE_NAME and GetCurrentEnv().FILE_NAME:gsub(".lua", "") or "WebSprites"
-    if FileExist(SPRITE_PATH .. env .. "\\" .. filename) then
-        sprite = createSprite(env .. "\\" .. filename)
+    if env and env:lower():sub(1, #("protected")) == "protected" then env = "Protected" end
+    local filepath = type(path)=="string" and path or (SPRITE_PATH .. env .. "\\" .. filename)
+    if FileExist(filepath) then
+        sprite = createSprite(filepath)
         if type(callback) == "function" then callback(sprite) end
     else
         if type(callback) == "function" then
-            MakeSurePathExists(SPRITE_PATH .. env .. "\\" .. filename)
-            DownloadFile(url, SPRITE_PATH .. env .. "\\" .. filename, function()
-                if FileExist(SPRITE_PATH .. env .. "\\" .. filename) then
-                    sprite = createSprite(env .. "\\" .. filename)
+            MakeSurePathExists(filepath)
+            DownloadFile(url, filepath, function()
+                if FileExist(filepath) then
+                    sprite = createSprite(filepath)
                 end
                 callback(sprite)
             end)
@@ -932,13 +934,13 @@ function GetWebSprite(url, callback)
             sprite = GetWebSprite(url, function(data)
                 finished = true
                 sprite = data
-            end)
-            while not (finished or sprite or FileExist(SPRITE_PATH .. env .. "\\" .. filename)) do
+            end, path)
+            while not (finished or sprite or FileExist(filepath)) do
                 RunCmdCommand("ping 127.0.0.1 -n 1 -w 1")
             end
         end
-        if not sprite and FileExist(SPRITE_PATH .. env .. "\\" .. filename) then
-            sprite = createSprite(env .. "\\" .. filename)
+        if not sprite and FileExist(filepath) then
+            sprite = createSprite(filepath)
         end
     end
     return sprite
